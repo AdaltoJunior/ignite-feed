129aabaf3281a274d014ef50f7fc8e06424e376e
Adalto Junior <mendesrochajunior@gmail.com>
2022-10-21 10:57:59 -0300

    first commit

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,26 @@
+import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
+
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img className={styles.cover} src="https://picsum.photos/id/299/500/500" alt="" />
+
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/diego3g.png" />
+
+        <strong>Diego Fernandes</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
